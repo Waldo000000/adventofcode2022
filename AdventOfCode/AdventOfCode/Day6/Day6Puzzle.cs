c49e7111faf433874b9c6e6d71f86d0799d31eef
Roy Wallace <waldo000000@gmail.com>
2022-12-10 10:50:13 +1000

    Day 6 - Part 1

---
+++ AdventOfCode/AdventOfCode/Day6/Day6Puzzle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day6;
+
+public static class Day6Puzzle
+{
+    private const int StartOfPacketMarkerLength = 4;
+
+    public static (int, bool) GetCharactersProcessedBeforeStartOfPacketMarker(StreamReader streamReader)
+    {
+        var buff = new List<char>();
+        var numRead = 0;
+        while (!IsStartOfPacket(buff) && !streamReader.EndOfStream)
+        {
+            buff = AddCharToBuffer(buff, streamReader.Read());
+            numRead++;
+        }
+
+        return (numRead, IsStartOfPacket(buff));
+    }
+
+    private static List<char> AddCharToBuffer(List<char> buff, int ch)
+    {
+        return buff.Append((char) ch).TakeLast(StartOfPacketMarkerLength).ToList();
+    }
+
+    private static bool IsStartOfPacket(List<char> buff)
+    {
+        return buff.Count == StartOfPacketMarkerLength &&
+               buff.Distinct().Count() == StartOfPacketMarkerLength;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/Day6/Day6Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using AdventOfCode.Day6;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Day6;
+
+public class Day6Tests
+{
+    [Test]
+    public void GetCharactersProcessedBeforeStartOfPacketMarker_WithSampleData_ReturnsExpectedResult()
+    {
+        using StreamReader sr = new StreamReader("Day6\\Part1.sample.txt");
+        Day6Puzzle.GetCharactersProcessedBeforeStartOfPacketMarker(sr).Should().Be((7, true));
+    }
+    
+    [Test]
+    public void GetCharactersProcessedBeforeStartOfPacketMarker_WithRealData_ReturnsExpectedResult()
+    {
+        using StreamReader sr = new StreamReader("Day6\\Part1.real.txt");
+        Day6Puzzle.GetCharactersProcessedBeforeStartOfPacketMarker(sr).Should().Be((1987, true));
+    }
+}
