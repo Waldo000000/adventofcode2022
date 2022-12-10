bd7460ac1cdb1e0cdb5fcef2fd026e48252c5c9e
Roy Wallace <waldo000000@gmail.com>
2022-12-10 17:54:36 +1000

    Day 7 - Part 1 input model

---
+++ AdventOfCode/AdventOfCode/Day7/Day7Puzzle.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Day7;
+
+public class Day7Puzzle
+{
+    public static int GetSumOfSizesOfDirectories(TerminalOutput terminalOutput)
+    {
+        // TODO: 
+        throw new System.NotImplementedException();
+    }
+}
+
+public record TerminalOutput(TerminalCommandOutput[] CommandOutputs);
+
+public class TerminalCommandOutput
+{
+}
+
+public class ChangeDirectoryTerminalCommandOutput : TerminalCommandOutput
+{
+    public ChangeDirectoryTerminalCommandOutput(string directoryName)
+    {
+        DirectoryName = directoryName;
+    }
+
+    public string DirectoryName { get; }
+}
+
+public class ListDirectoryContentsTerminalCommandOutput : TerminalCommandOutput
+{
+    public ListDirectoryContentsTerminalCommandOutput(DirectorySummary[] directories, FileSummary[] files)
+    {
+        Directories = directories;
+        Files = files;
+    }
+
+    public DirectorySummary[] Directories { get; }
+    
+    public FileSummary[] Files { get; }
+}
+
+public record FileSummary(string Name, int Size);
+
+public record DirectorySummary(string Name);
---
+++ AdventOfCode/AdventOfCodeTests/Day7/Day7Tests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using AdventOfCode.Day7;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Day7;
+
+public class Day7Tests
+{
+    [Test]
+    public void GetSumOfSizesOfDirectories_WithMaxSize100000AndSampleData_ReturnsExpectedResult()
+    {
+        var terminalOutput = ParseTerminalOutput();
+        Day7Puzzle.GetSumOfSizesOfDirectories(terminalOutput).Should().Be(95437);
+    }
+
+    private static TerminalOutput ParseTerminalOutput()
+    {
+        // TODO: 
+        // var lines = File.ReadAllLines("Day7\\Part1.sample.txt");
+        // ...
+
+        var terminalOutput = new TerminalOutput(
+            new TerminalCommandOutput[]
+            {
+                new ChangeDirectoryTerminalCommandOutput("/"),
+                new ListDirectoryContentsTerminalCommandOutput(new[]
+                    {
+                        new DirectorySummary("a"),
+                        new DirectorySummary("a"),
+                    },
+                    new[]
+                    {
+                        new FileSummary("b.txt", 14848514),
+                        new FileSummary("c.dat", 8504156),
+                    })
+            });
+        return terminalOutput;
+    }
+
+    [Test]
+    public void GetSumOfSizesOfDirectories_WithMaxSize100000AndRealData_ReturnsExpectedResult()
+    {
+    }
+}
