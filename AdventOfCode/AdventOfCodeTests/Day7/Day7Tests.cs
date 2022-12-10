4f2a210bcfbc62414b69aa8903478b4bae4049b3
Roy Wallace <waldo000000@gmail.com>
2022-12-10 19:57:11 +1000

    Day 7 - Part 1 sample

--- AdventOfCode/AdventOfCodeTests/Day7/Day7Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day7/Day7Tests.cs
@@ -1,43 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using AdventOfCode.Day7;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace AdventOfCodeTests.Day7;
 
 public class Day7Tests
 {
+    private static readonly Regex DirectorySummaryRegex = new Regex(@"^dir ([\w]*)$");
+    private static readonly Regex FileSummaryRegex = new Regex(@"^([\d]+) (.*)$");
+    private static readonly Regex CommandWithOutputRegex = new Regex(@"\$[^\$]*");
+    private static readonly Regex RootDirectoryCommandRegex = new Regex(@"\$ cd /");
+    private static readonly Regex UpDirectoryCommandRegex = new Regex(@"\$ cd \.\.");
+    private static readonly Regex RelativeChangeDirectoryCommandRegex = new Regex(@"\$ cd ([\w]+)");
+    private static readonly Regex ListDirectoryContentsCommandRegex = new Regex(@"\$ ls", RegexOptions.Multiline);
+
     [Test]
     public void GetSumOfSizesOfDirectories_WithMaxSize100000AndSampleData_ReturnsExpectedResult()
     {
-        var terminalOutput = ParseTerminalOutput();
+        var terminalOutput = ParseTerminalOutput().ToArray();
         Day7Puzzle.GetSumOfSizesOfDirectories(terminalOutput, 100000).Should().Be(95437);
     }
 
-    private static Command[] ParseTerminalOutput()
+    private static IEnumerable<Command> ParseTerminalOutput()
     {
-        // TODO: 
-        // var lines = File.ReadAllLines("Day7\\Part1.sample.txt");
-        // ...
+        var text = System.IO.File.ReadAllText("Day7\\Part1.sample.txt");
 
-        return new Command[]
+        return CommandWithOutputRegex.Matches(text).Select(ParseCommand);
+    }
+
+    private static Command ParseCommand(Match commandWithOutput)
+    {
+        if (RootDirectoryCommandRegex.IsMatch(commandWithOutput.Value))
+            return new RootDirectoryCommand();
+        if (UpDirectoryCommandRegex.IsMatch(commandWithOutput.Value))
+            return new UpDirectoryCommand();
+        if (RelativeChangeDirectoryCommandRegex.IsMatch(commandWithOutput.Value))
+            return new RelativeChangeDirectoryCommand(RelativeChangeDirectoryCommandRegex
+                .Match(commandWithOutput.Value).Groups[1].ToString());
+        if (ListDirectoryContentsCommandRegex.IsMatch(commandWithOutput.Value))
         {
-            new RootDirectoryCommand(),
-            new ListDirectoryContentsCommand(new[]
+            var summaries = commandWithOutput.Value
+                .Split(Environment.NewLine)
+                .Skip(1)
+                .ToList();
+
+            var directories = summaries
+                .Where(summary => DirectorySummaryRegex.IsMatch(summary))
+                .Select(summary => new DirectorySummary(DirectorySummaryRegex.Match(summary).Groups[1].ToString()))
+                .ToArray();
+
+            var files = summaries
+                .Where(summary => FileSummaryRegex.IsMatch(summary))
+                .Select(summary =>
                 {
-                    new DirectorySummary("a"),
-                },
-                new[]
-                {
-                    new FileSummary("b.txt", 14848514),
-                    new FileSummary("c.dat", 8504156),
-                }),
-            new RelativeChangeDirectoryCommand("a"),
-            new ListDirectoryContentsCommand(Array.Empty<DirectorySummary>(), new []
-            {
-                new FileSummary("x.txt", 90000)
-            })
-        };
+                    var match = FileSummaryRegex.Match(summary);
+                    return new FileSummary(
+                        match.Groups[2].ToString(),
+                        Convert.ToInt32(match.Groups[1].Value)
+                    );
+                })
+                .ToArray();
+
+            return new ListDirectoryContentsCommand(directories, files);
+        }
+
+        throw new Exception($"Couldn't parse command: {commandWithOutput}");
     }
 
     [Test]
