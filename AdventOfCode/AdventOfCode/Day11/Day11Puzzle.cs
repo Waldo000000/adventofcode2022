ef9e77ad4186f2f6869fd9a97f8a30628fb2efa5
Roy Wallace <waldo000000@gmail.com>
2022-12-12 17:59:53 +1000

    Day 11 - Part 1

--- AdventOfCode/AdventOfCode/Day11/Day11Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day11/Day11Puzzle.cs
@@ -1,23 +1,91 @@
 ﻿using System.Collections.Generic;
-using NCalc;
+using System.Linq;
+using Expression = NCalc.Expression;
 
 namespace AdventOfCode.Day11;
 
 public static class Day11Puzzle
 {
-    public static int GetLevelOfMonkeyBusiness(Monkey[] monkeys)
+    public static int GetLevelOfMonkeyBusinessAfterRounds(Monkey[] monkeys, int numRounds)
     {
-        throw new System.NotImplementedException();
+        for (int round = 0; round < numRounds; round++)
+        {
+            monkeys.ToList().ForEach(monkey => { monkey.InspectItemsThenThrowTo(monkeys); });
+        }
+
+        return GetMonkeyBusiness(monkeys);
     }
-}i
+
+    private static int GetMonkeyBusiness(Monkey[] monkeys)
+    {
+        return monkeys
+            .OrderByDescending(m => m.NumInspectedItems)
+            .Take(2)
+            .Select(m => m.NumInspectedItems)
+            .Aggregate((x, y) => x * y);
+    }
+}
 
 public record Monkey(
     int Id,
-    List<int> Items,
-    Expression Expression,
-    int DivisibleByTest,
+    List<Item> Items,
+    Expression NewWorryLevelAfterInspectionOperation,
+    int NewWorryLevelIsDivisibleByTest,
     int IfTestIsTrueThrowToMonkeyId,
     int IfTestIsFalseThrowToMonkeyId
-);
+)
+{
+    private const string OldWorryLevelExpressionName = "old";
+    public int NumInspectedItems { get; private set; }
 
-public record Item();
+    public void InspectItemsThenThrowTo(Monkey[] monkeys)
+    {
+        var oldItems = Items.ToList();
+        oldItems.ToList().ForEach(item => InspectItemThenThrowTo(monkeys, item));
+    }
+
+    private void InspectItemThenThrowTo(Monkey[] monkeys, Item item)
+    {
+        NumInspectedItems++;
+        UpdateWorryLevelAfterInspection(item);
+        var toMonkeyId = GetThrowToMonkeyId(item);
+        ThrowItemTo(monkeys, toMonkeyId, item);
+    }
+
+    private void UpdateWorryLevelAfterInspection(Item item)
+    {
+        NewWorryLevelAfterInspectionOperation.Parameters[OldWorryLevelExpressionName] = item.WorryLevel;
+        var newWorryLevel = (int) NewWorryLevelAfterInspectionOperation.Evaluate();
+        item.WorryLevel = newWorryLevel / 3;
+    }
+
+    private int GetThrowToMonkeyId(Item item)
+    {
+        return item.WorryLevel % NewWorryLevelIsDivisibleByTest == 0
+            ? IfTestIsTrueThrowToMonkeyId
+            : IfTestIsFalseThrowToMonkeyId;
+    }
+
+    private void ThrowItemTo(Monkey[] monkeys, int toMonkeyId, Item item)
+    {
+        var toMonkey = GetMonkeyWithId(monkeys, toMonkeyId);
+
+        toMonkey.Items.Add(item);
+        Items.Remove(item);
+    }
+
+    private static Monkey GetMonkeyWithId(Monkey[] monkeys, int toMonkeyId)
+    {
+        return monkeys.Single(m => m.Id == toMonkeyId);
+    }
+}
+
+public class Item
+{
+    public Item(int worryLevel)
+    {
+        WorryLevel = worryLevel;
+    }
+
+    public int WorryLevel { get; set; }
+};
--- AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
@@ -15,21 +15,32 @@ public class Day11Tests
     {
         var monkeys = ReadMonkeys("Day11\\Part1.sample.txt").ToArray();
 
-        Day11Puzzle.GetLevelOfMonkeyBusiness(monkeys).Should().Be(10605);
+        Day11Puzzle.GetLevelOfMonkeyBusinessAfterRounds(monkeys, 20).Should().Be(10605);
+    }
+
+    [Test] public void GetLevelOfMonkeyBusiness_After20RoundsWithRealData_ReturnsExpectedValues()
+    {
+        var monkeys = ReadMonkeys("Day11\\Part1.real.txt").ToArray();
+
+        Day11Puzzle.GetLevelOfMonkeyBusinessAfterRounds(monkeys, 20).Should().Be(10605);
     }
 
     private IEnumerable<Monkey> ReadMonkeys(string filename)
     {
         return File.ReadAllLines(filename)
             .Chunk(7)
-            .Select(ch => ParseMonkey(ch))
+            .Select(ParseMonkey)
             .ToList();
     }
 
     private Monkey ParseMonkey(string[] lines)
     {
         var id = int.Parse(lines[0].Split("Monkey ")[1].Trim(':'));
-        var items = lines[1].Split(": ")[1].Split(", ").Select(int.Parse).ToList();
+        var items = lines[1].Split(": ")[1].Split(", ")
+            .Select(int.Parse)
+            .Select(worryLevel => new Item(worryLevel))
+            .ToList();
+
         var operation = new Expression(lines[2].Split("new = ")[1]);
         var divisibleByTest = int.Parse(lines[3].Split("divisible by ")[1]);
         var ifTestIsTrueThrowToMonkeyId = int.Parse(lines[4].Split("throw to monkey ")[1]);
