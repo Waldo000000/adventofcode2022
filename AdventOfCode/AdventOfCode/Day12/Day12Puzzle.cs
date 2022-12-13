a2449b7d3e61df616a9b9d0ddae1a70f717bc543
Roy Wallace <waldo000000@gmail.com>
2022-12-13 20:46:35 +1000

    Day 12 - Part 1

--- AdventOfCode/AdventOfCode/Day12/Day12Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day12/Day12Puzzle.cs
@@ -1,4 +1,8 @@
-﻿using AdventOfCode.Day9;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Day9;
+using Dijkstra.NET.Graph.Simple;
+using Dijkstra.NET.ShortestPath;
 
 namespace AdventOfCode.Day12;
 
@@ -6,9 +10,79 @@ public static class Day12Puzzle
 {
     public static int GetFewestStepsToBestSignal(Coord currentPosition, Coord bestSignalPosition, Heightmap heightmap)
     {
-        // TODO
-        throw new System.NotImplementedException();
+        var (nodeIds, graph) = CreateGraph(heightmap);
+        var from = nodeIds[currentPosition];
+        var to = nodeIds[bestSignalPosition];
+        var result = graph.Dijkstra(from, to);
+        return result.Distance;
+    }
+
+    private static (Dictionary<Coord, uint> nodeIds, Graph graph) CreateGraph(Heightmap heightmap)
+    {
+        var graph = new Graph();
+        var nodeIds = CreateNodes(heightmap, graph);
+
+        Enumerable.Range(0, heightmap.GetMaxX()).ToList().ForEach(xIdx =>
+        {
+            Enumerable.Range(0, heightmap.GetMaxY()).ToList().ForEach(yIdx =>
+            {
+                var fromCoord = new Coord(xIdx, yIdx);
+
+                heightmap
+                    .GetAdjacentCoords(fromCoord)
+                    .Where(adjCoord => heightmap.CanTraverse(fromCoord, adjCoord))
+                    .ToList().ToList().ForEach(accessibleCoord =>
+                    {
+                        graph.Connect(nodeIds[fromCoord], nodeIds[accessibleCoord], 1);
+                    });
+            });
+        });
+
+        return (nodeIds, graph);
+    }
+
+    private static Dictionary<Coord, uint> CreateNodes(Heightmap heightmap, Graph graph)
+    {
+        var nodeIds = new Dictionary<Coord, uint>();
+
+        Enumerable.Range(0, heightmap.GetMaxX()).ToList().ForEach(xIdx =>
+        {
+            Enumerable.Range(0, heightmap.GetMaxY()).ToList().ForEach(yIdx =>
+            {
+                var nodeKey = graph.AddNode();
+                nodeIds[new Coord(xIdx, yIdx)] = nodeKey;
+            });
+        });
+
+        return nodeIds;
     }
 }
 
-public record Heightmap(int[][] Heights);
+public record Heightmap(int[][] Heights)
+{
+    public bool CanTraverse(Coord from, Coord to)
+    {
+        return GetHeight(to) <= GetHeight(from) + 1;
+    }
+
+    public IEnumerable<Coord> GetAdjacentCoords(Coord from)
+    {
+        if (from.X > 0)
+            yield return new Coord(from.X - 1, from.Y);
+        if (from.Y < GetMaxY() - 1)
+            yield return new Coord(from.X, from.Y + 1);
+        if (from.X < GetMaxX() - 1)
+            yield return new Coord(from.X + 1, from.Y);
+        if (from.Y > 0)
+            yield return new Coord(from.X, from.Y - 1);
+    }
+
+    private int GetHeight(Coord coord)
+    {
+        return Heights[coord.X][coord.Y];
+    }
+
+    public int GetMaxY() => Heights.First().Length;
+
+    public int GetMaxX() => Heights.Length;
+};
--- AdventOfCode/AdventOfCodeTests/Day12/Day12Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day12/Day12Tests.cs
@@ -6,25 +6,25 @@ using AdventOfCode.Day9;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace AdventOfCodeTests.Day11;
+namespace AdventOfCodeTests.Day12;
 
 public class Day12Tests
 {
-    private static readonly char[] Alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    private static readonly char[] EncodedHeightsAscending = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
 
     [Test]
     public void GetFewestStepsToBestSignal_WithSampleData_ReturnsExpectedValue()
     {
         var (currentPosition, bestSignalPosition, heightmap) = ReadHeightmap("Day12\\Part1.sample.txt");
 
-        Day12Puzzle.GetFewestStepsToBestSignal(currentPosition, bestSignalPosition, heightmap).Should().Be(10605);
+        Day12Puzzle.GetFewestStepsToBestSignal(currentPosition, bestSignalPosition, heightmap).Should().Be(31);
     }
 
     [Test] public void GetFewestStepsToBestSignal_WithRealData_ReturnsExpectedValue()
     {
-        var (currentPosition, bestSignalPosition, heightmap) = ReadHeightmap("Day12\\Part1.sample.txt");
+        var (currentPosition, bestSignalPosition, heightmap) = ReadHeightmap("Day12\\Part1.real.txt");
 
-        Day12Puzzle.GetFewestStepsToBestSignal(currentPosition, bestSignalPosition, heightmap).Should().Be(-1); // TODO
+        Day12Puzzle.GetFewestStepsToBestSignal(currentPosition, bestSignalPosition, heightmap).Should().Be(481);
     }
 
     private (Coord, Coord, Heightmap) ReadHeightmap(string filename)
@@ -64,8 +64,8 @@ public class Day12Tests
         return ch switch
         {
             'S' => 0,
-            'E' => Alphabet.Length - 1,
-            _ => Alphabet.ToList().FindIndex(c => c == ch)
+            'E' => EncodedHeightsAscending.Length - 1,
+            _ => EncodedHeightsAscending.ToList().FindIndex(c => c == ch)
         };
     }
 }
