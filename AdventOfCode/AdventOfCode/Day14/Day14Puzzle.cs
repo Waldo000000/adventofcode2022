cb8dc003afdb5ac0280cf809a014291c19ae45fe
Roy Wallace <waldo000000@gmail.com>
2022-12-16 16:17:28 +1000

    Day 14 - stub

---
+++ AdventOfCode/AdventOfCode/Day14/Day14Puzzle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventOfCode.Day9;
+
+namespace AdventOfCode.Day14;
+
+public static class Day14Puzzle
+{
+    public static int GetFinalCountOfRestingSand(Coord sandOrigin, RockPath[] rockPaths)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public record RockPath(params Coord[] points);
---
+++ AdventOfCode/AdventOfCodeTests/Day14/Day14Tests.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Day13;
+using AdventOfCode.Day14;
+using AdventOfCode.Day9;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Day14;
+
+public class Day14Tests
+{
+    [Test]
+    public void GetFinalCountOfRestingSand_WithSampleData_ReturnsExpectedValue()
+    {
+        var rockPaths = ReadRockPaths("Day13\\Part1.sample.txt").ToArray();
+
+        var sandOrigin = new Coord(500,0);
+        
+        Day14Puzzle.GetFinalCountOfRestingSand(sandOrigin, rockPaths).Should().Be(24);
+    }
+
+    [Test]
+    public void GetSumOfIndicesOfOrderedPairs_WithRealData_ReturnsExpectedValue()
+    {
+        var rockPaths = ReadRockPaths("Day13\\Part1.real.txt").ToArray();
+
+        var sandOrigin = new Coord(500,0);
+
+        Day14Puzzle.GetFinalCountOfRestingSand(sandOrigin, rockPaths).Should().Be(24);
+    }
+
+    private RockPath[] ReadRockPaths(string filename)
+    {
+        return new[]
+        {
+            new RockPath(new Coord(498, 4), new Coord(498, 6), new Coord(496, 6)),
+            new RockPath(new Coord(503, 4), new Coord(502, 4), new Coord(502, 9), new Coord(494, 9))
+        };
+
+        // TODO: string parsing
+    }
+}
